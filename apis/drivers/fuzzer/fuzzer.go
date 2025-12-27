e327dddb293b63fd877cb4fb73d4c3a9f7b7a0d1
Tamal Saha <tamal@appscode.com>
2025-12-27 11:23:22 +0600

    Use k8s 1.34 client libs (#7)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/drivers/fuzzer/fuzzer.go
+++ apis/drivers/fuzzer/fuzzer.go
@@ -17,17 +17,17 @@ limitations under the License.
 package fuzzer
 
 import (
+	"sigs.k8s.io/randfill"
 	"x-helm.dev/apimachinery/apis/drivers/v1alpha1"
 
-	fuzz "github.com/google/gofuzz"
 	runtimeserializer "k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
 // Funcs returns the fuzzer functions for this api group.
-var Funcs = func(codecs runtimeserializer.CodecFactory) []interface{} {
-	return []interface{}{
-		func(s *v1alpha1.AppRelease, c fuzz.Continue) {
-			c.FuzzNoCustom(s) // fuzz self without calling this function again
+var Funcs = func(codecs runtimeserializer.CodecFactory) []any {
+	return []any{
+		func(s *v1alpha1.AppRelease, c randfill.Continue) {
+			c.Fill(s) // fuzz self without calling this function again
 		},
 	}
 }
